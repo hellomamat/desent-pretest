555b8350e848eb510a7bb62d2c58385c3351c725
hellorahmat <rizkyrahmat198@gmail.com>
2026-03-02 10:29:38 +0700

    2

--- handlers/echo.go
+++ handlers/echo.go
@@ -4,12 +4,6 @@ import "github.com/gofiber/fiber/v2"
 
 // Level 2: Echo
 func Echo(c *fiber.Ctx) error {
-	var body map[string]interface{}
-	if err := c.BodyParser(&body); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"success": false,
-			"message": "invalid JSON body",
-		})
-	}
-	return c.JSON(body)
+	c.Set("Content-Type", "application/json")
+	return c.Send(c.Body())
 }
